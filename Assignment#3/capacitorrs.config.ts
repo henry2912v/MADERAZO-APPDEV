0221ee31b3fd7d8de7d3837b4568c0bb22a0fe8f
henry2912v <henrymaderazo19@gmail.com>
2024-03-30 10:03:21 +0800

    first commit

--- Assignment#3/capacitorrs.config.ts
+++ Assignment#3/capacitorrs.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'BSIT-KAME',
+  appName: 'regir, maderazo, marcos-assignment-3',
   webDir: 'www',
   server: {
     androidScheme: 'https'
